26cb21b3685d21e28b4ff8f687fc3409e8c39020
ashadi <ashadi.cc@gmail.com>
2022-02-23 10:58:36 +0700

    update clip example

--- v2/example/clip/main.go
+++ v2/example/clip/main.go
@@ -17,60 +17,60 @@ func main() {
 	// get values from environment variable
 	voiceUUID := example.LoadConfigByKey("TEST_VOICE_UUID")
 	projectUUID := example.LoadConfigByKey("TEST_PROJECT_UUID")
-	// callbackUrl := example.LoadConfigByKey("TEST_CALLBACK_URL")
+	callbackUrl := example.LoadConfigByKey("TEST_CALLBACK_URL")
 
-	// // create sync
-	// clip, err := client.Clip.CreateSync(projectUUID, request.Payload{
-	// 	"body":       "this is test.",
-	// 	"voice_uuid": voiceUUID,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(clip.Success)
+	// create sync
+	clip, err := client.Clip.CreateSync(projectUUID, request.Payload{
+		"body":       "this is test.",
+		"voice_uuid": voiceUUID,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(clip.Success)
 
-	// // craete async clip
-	// time.Sleep(time.Second)
-	// clip2, err := client.Clip.CreateAsync(projectUUID, callbackUrl, request.Payload{
-	// 	"voice_uuid": voiceUUID,
-	// 	"body":       "test async.",
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("%+v \n", clip2)
+	// craete async clip
+	time.Sleep(time.Second)
+	clip2, err := client.Clip.CreateAsync(projectUUID, callbackUrl, request.Payload{
+		"voice_uuid": voiceUUID,
+		"body":       "test async.",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v \n", clip2)
 
-	// // get clip by uuid
-	// time.Sleep(time.Second)
-	// clip, err = client.Clip.Get(projectUUID, clip.Item.UUID)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(clip.Success)
+	// get clip by uuid
+	time.Sleep(time.Second)
+	clip, err = client.Clip.Get(projectUUID, clip.Item.UUID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(clip.Success)
 
-	// // get all clips
-	// time.Sleep(time.Second)
-	// clips, err := client.Clip.All(projectUUID, 2)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("%+v \n", clips.Items)
+	// get all clips
+	time.Sleep(time.Second)
+	clips, err := client.Clip.All(projectUUID, 2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v \n", clips.Items)
 
-	// // update clip
-	// time.Sleep(time.Second)
-	// clip, err = client.Clip.UpdateAsync(projectUUID, clip.Item.UUID, callbackUrl, request.Payload{"body": "test update.", "voice_uuid": voiceUUID})
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(clip.Success)
+	// update clip
+	time.Sleep(time.Second)
+	clip, err = client.Clip.UpdateAsync(projectUUID, clip.Item.UUID, callbackUrl, request.Payload{"body": "test update.", "voice_uuid": voiceUUID})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(clip.Success)
 
-	// // delete clip
-	// time.Sleep(time.Second)
-	// m, err := client.Clip.Delete(projectUUID, clip.Item.UUID)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(m.Success)
+	// delete clip
+	time.Sleep(time.Second)
+	m, err := client.Clip.Delete(projectUUID, clip.Item.UUID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(m.Success)
 
 	// stream clip
 	time.Sleep(time.Second)
